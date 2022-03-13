cb669338acbe5e03ffcea70bf1be20f223cef520
Mhsalameh <mohsalameh1@gmail.com>
2022-03-13 10:22:07 +0300

    challenge 08 completed

--- javascript/code-challenges/linked-list/README.md
+++ javascript/code-challenges/linked-list/README.md
@@ -1,22 +1,31 @@
 ## Challenge Summary
+
 - Linked-list: Create a linkedList class that has 3 methods:
-    1. insert: insert a new node to the linked list.
-    2. includes: check if the linked list includes a value and return true if it's include and false if not.
-    3. toString: retun a string that shows the components of the linked list as <br/>
+  1. insert: insert a new node to the linked list.
+  2. includes: check if the linked list includes a value and return true if it's include and false if not.
+  3. toString: retun a string that shows the components of the linked list as
 - linked-list-insertions: insert with 3 methods:
-    1. append: insert a new node to the end of linked list.
-    2. insertAfter: add a node after a specific value.
-    3. insertBefroe: add a node before a specific value<br/>
+  1. append: insert a new node to the end of linked list.
+  2. insertAfter: add a node after a specific value.
+  3. insertBefroe: add a node before a specific value
 - linked-list-kth: return the value that counting k from the end of the list
+- linked-list-zip : Zip the two linked lists together into one so that the nodes alternate between the two lists and return a reference to the the zipped list.
 
 ## Whiteboard Process
+
 - linked-list-insertions:
-![](./assets/class-06.png)
+  ![linked-list](./assets/class-06.png)
 - linked-list-kth:
-![](./assets/class-07.png)
+  ![linked-list-kth](./assets/class-07.png)
+- linked-list-zip
+  ![linked-list-zip](./assets/class-08.png)
+
 ## Approach & Efficiency
+
 Big O -> time complexity O(1), Big O -> space complexity O(n)
+
 ## API
+
 1. insert: insert a new node to the linked list.
 2. includes: check if the linked list includes a value and return true if it's include and false if not.
 3. toString: retun a string that shows the components of the linked list as string
@@ -24,12 +33,16 @@ Big O -> time complexity O(1), Big O -> space complexity O(n)
 5. insertAfter: add a node after a specific value.
 6. insertBefroe: add a node before a specific value
 7. kthFromEnd: return the value that is k steps away from the end of list
+8. zipLists: return a zipped list;
+
 ## solution
+
 - create a new instance with LinkedList class add to it a value `let list = new LinkedList(value)`
 - to add a new node to the start of the list with value use `list.insert(value)`
 - to check if the list includes a value `list.includes(value)`
 - to strigify the list and return it like "{a}->{b}->{c}->null" `list.toString()`
 - to add a new node to the end of the list with value use `list.append(value)`
-- to add a node before a value use `insertBefore(value,newValue)`
-- to add a node after a value use `instertAfter(value,newValue)` 
+- to add a node before a value use `list.insertBefore(value,newValue)`
+- to add a node after a value use `list.instertAfter(value,newValue)`
 - to return the kth value from the end of the list use `list.kthFromEnd(k)`
+- to return a zip of two links use `zipLists(list1,list2);`
--- javascript/code-challenges/linked-list/__tests__/ll.test.js
+++ javascript/code-challenges/linked-list/__tests__/ll.test.js
@@ -1,126 +1,143 @@
-"use strict";
-const { it } = require("eslint/lib/rule-tester/rule-tester");
-const LinkedList = require("../ll.js");
+'use strict';
+const { it } = require('eslint/lib/rule-tester/rule-tester');
+const { LinkedList, zipLists } = require('../ll.js');
 
+describe('testing linkedlist', () => {
+  let consoleSpy;
 
-describe("testing linkedlist", () => {
-    
-    let consoleSpy; 
+  beforeEach(() => {
+    consoleSpy = jest.spyOn(console, 'error').mockImplementation();
+  });
+  afterEach(() => {
+    consoleSpy.mockRestore();
+  });
 
-    beforeEach( ()=>{
-        consoleSpy =  jest.spyOn(console, 'error').mockImplementation();
-    })
-    afterEach(()=>{
-        consoleSpy.mockRestore();
-    })
-
-  it("testing creating new linkedlist", () => {
+  it('testing creating new linkedlist', () => {
     let newLink = new LinkedList();
     // console.log(newLink);
     expect(newLink).toBeInstanceOf(LinkedList);
     expect(newLink.head).toBeNull();
   });
 
-  it("testing inserting new node", () => {
+  it('testing inserting new node', () => {
     let newLink = new LinkedList();
-    newLink.insert("a");
-    expect(newLink.head.value).toEqual("a");
+    newLink.insert('a');
+    expect(newLink.head.value).toEqual('a');
     expect(newLink.head.next).toBeNull();
     // console.log(newLink.head)
-    newLink.insert("b");
+    newLink.insert('b');
     // console.log(newLink)
-    expect(newLink.head.value).toEqual("b");
-    expect(newLink.head.next.value).toEqual("a");
+    expect(newLink.head.value).toEqual('b');
+    expect(newLink.head.next.value).toEqual('a');
     expect(newLink.head.next.next).toBeNull();
   });
-  it("testing include method in linked list", () => {
+  it('testing include method in linked list', () => {
     let newLink = new LinkedList();
-    newLink.insert("a");
-    newLink.insert("b");
-    newLink.insert("c");
-    expect(newLink.includes("a")).toBeTruthy();
-    expect(newLink.includes("b")).toBeTruthy();
-    expect(newLink.includes("c")).toBeTruthy();
-    expect(newLink.includes("g")).toBeFalsy();
+    newLink.insert('a');
+    newLink.insert('b');
+    newLink.insert('c');
+    expect(newLink.includes('a')).toBeTruthy();
+    expect(newLink.includes('b')).toBeTruthy();
+    expect(newLink.includes('c')).toBeTruthy();
+    expect(newLink.includes('g')).toBeFalsy();
   });
-  it("testing toString method in linked list", () => {
+  it('testing toString method in linked list', () => {
     let newLink = new LinkedList();
     expect(newLink.toString()).toEqual(null);
-    newLink.insert("a");
-    expect(newLink.toString()).toEqual("{a}-> null");
-    newLink.insert("b");
-    expect(newLink.toString()).toEqual("{b}-> {a}-> null");
+    newLink.insert('a');
+    expect(newLink.toString()).toEqual('{a}-> null');
+    newLink.insert('b');
+    expect(newLink.toString()).toEqual('{b}-> {a}-> null');
   });
 
-  it("Testing initiating linked list", () => {
+  it('Testing initiating linked list', () => {
     const list = new LinkedList();
     expect(list).toBeInstanceOf(LinkedList);
     expect(list.head).toBeNull();
   });
 
-  it("Testing append : adding node to the end of the list", () => {
+  it('Testing append : adding node to the end of the list', () => {
     const list = new LinkedList();
-    list.append("a");
-    expect(list.head.value).toEqual("a");
+    list.append('a');
+    expect(list.head.value).toEqual('a');
     expect(list.head.next).toBeNull();
-    list.append("b");
-    expect(list.head.value).toEqual("a");
-    expect(list.head.next.value).toEqual("b");
+    list.append('b');
+    expect(list.head.value).toEqual('a');
+    expect(list.head.next.value).toEqual('b');
     expect(list.head.next.next).toBeNull();
   });
 
-  it("testing insertBefore: add a node before a specific value", () => {
+  it('testing insertBefore: add a node before a specific value', () => {
     const list = new LinkedList();
-    list.insertBefore("a", "b");
-    expect(consoleSpy).toHaveBeenCalledWith("Error: linked list is empty");
-    list.append("a");
-    list.insertBefore("a", "b");
-    expect(list.head.value).toEqual("b");
-    expect(list.head.next.value).toEqual("a");
+    list.insertBefore('a', 'b');
+    expect(consoleSpy).toHaveBeenCalledWith('Error: linked list is empty');
+    list.append('a');
+    list.insertBefore('a', 'b');
+    expect(list.head.value).toEqual('b');
+    expect(list.head.next.value).toEqual('a');
     expect(list.head.next.next).toBeNull;
-    list.insertBefore("a", "c");
-    expect(list.head.next.value).toEqual("c");
-    expect(list.head.next.next.value).toEqual("a");
+    list.insertBefore('a', 'c');
+    expect(list.head.next.value).toEqual('c');
+    expect(list.head.next.next.value).toEqual('a');
     expect(list.head.next.next.next).toBeNull();
-    list.insertBefore("j", "t");
+    list.insertBefore('j', 't');
     expect(consoleSpy).toHaveBeenCalledWith(
-      "Error: j is not available in the linked list"
+      'Error: j is not available in the linked list'
     );
   });
 
-  it("testing insertAfter: add a node after a specific value", () => {
+  it('testing insertAfter: add a node after a specific value', () => {
     const list = new LinkedList();
-    list.insertAfter("a", "b");
-    expect(consoleSpy).toHaveBeenCalledWith("Error: linked list is empty");
-    list.append("a");
-    list.insertAfter("a", "b");
-    expect(list.head.value).toEqual("a");
-    expect(list.head.next.value).toEqual("b");
+    list.insertAfter('a', 'b');
+    expect(consoleSpy).toHaveBeenCalledWith('Error: linked list is empty');
+    list.append('a');
+    list.insertAfter('a', 'b');
+    expect(list.head.value).toEqual('a');
+    expect(list.head.next.value).toEqual('b');
     expect(list.head.next.next).toBeNull;
-    list.insertAfter("b", "c");
-    expect(list.head.next.value).toEqual("b");
-    expect(list.head.next.next.value).toEqual("c");
+    list.insertAfter('b', 'c');
+    expect(list.head.next.value).toEqual('b');
+    expect(list.head.next.next.value).toEqual('c');
     expect(list.head.next.next.next).toBeNull();
-    list.insertAfter("j", "t");
+    list.insertAfter('j', 't');
     expect(consoleSpy).toHaveBeenCalledWith(
-      "Error: j is not available in the linked list"
+      'Error: j is not available in the linked list'
     );
   });
-  it("testing kthFromEnd",()=>{
+  it('testing kthFromEnd', () => {
     const list = new LinkedList();
     let kthValue;
     list.kthFromEnd(1);
-    expect(consoleSpy).toHaveBeenCalledWith("Error: linked list is empty")
+    expect(consoleSpy).toHaveBeenCalledWith('Error: linked list is empty');
     list.append('a');
-   kthValue= list.kthFromEnd(0);
-    expect(kthValue).toEqual('a')
-    kthValue=list.kthFromEnd(1);
-    expect(consoleSpy).toHaveBeenCalledWith(`argument:'k' can't be equal or more than 1: length of the list, k is limited from 0 to 0`)
+    kthValue = list.kthFromEnd(0);
+    expect(kthValue).toEqual('a');
+    kthValue = list.kthFromEnd(1);
+    expect(consoleSpy).toHaveBeenCalledWith(
+      `argument:'k' can't be equal or more than 1: length of the list, k is limited from 0 to 0`
+    );
     list.append('b');
-    kthValue=list.kthFromEnd(1);
-    expect(kthValue).toEqual('a')
-    kthValue=list.kthFromEnd(0);
-    expect(kthValue).toEqual('b')
-
-  })
+    kthValue = list.kthFromEnd(1);
+    expect(kthValue).toEqual('a');
+    kthValue = list.kthFromEnd(0);
+    expect(kthValue).toEqual('b');
+  });
+  it('testing Linked list zip', () => {
+    let list1 = new LinkedList();
+    let list2 = new LinkedList();
+    let list3 = new LinkedList();
+    list1.append('a');
+    expect(zipLists(list1, list2).toString()).toEqual('{a}-> null');
+    list2.append('b');
+    expect(zipLists(list1, list2).toString()).toEqual('{a}-> {b}-> null');
+    list2.append('c');
+    expect(zipLists(list1, list2).toString()).toEqual(
+      '{a}-> {b}-> {c}-> null'
+    );
+    list1.append('d');
+    expect(zipLists(list1, list2).toString()).toEqual(
+      '{a}-> {b}-> {d}-> {c}-> null'
+    );
+    expect(zipLists(list3, list1).toString()).toEqual('{a}-> {d}-> null');
+  });
 });
